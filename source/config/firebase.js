e8f0edd97578d2612523742b6bcc353ac0149b6d
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-05-14 15:06:25 -0500

    Obtenemos una instancia de Firestore que utilizaremos para interactuar con la base de datos.

---
+++ source/config/firebase.js
@@ -0,0 +1,21 @@
+// Importamos las funciones necesarias desde la biblioteca de firebase y Constants de expo.
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+// Configuración de firebase con los valores extra de Constants de Expo.
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  measurementId: Constants.manifest.extra.measurementId,
+};
+
+// Inicializamos Firebase con la configuración proporcionada.
+initializeApp(firebaseConfig);
+
+// Obtenemos una instancia de Firestore que utilizaremos para interactuar con la base de datos.
+export const baseDatos = getFirestore();
