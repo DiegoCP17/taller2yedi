6a3f357683a65b0b486c0bd791b9506f45278a19
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-05-14 16:17:01 -0500

    Se adiere Navigation para motrar

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./source/components/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>helloow MamaGuebo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
